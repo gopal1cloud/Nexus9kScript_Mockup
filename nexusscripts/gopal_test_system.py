89c1ea89d494fa0fcc3f93caba88aa9a5654ac8c
Gopalasivam <gopal@onecloudinc.com>
2015-06-09 19:15:15 +0530

    Update gopal_test_system.py

--- nexusscripts/gopal_test_system.py
+++ nexusscripts/gopal_test_system.py
@@ -4,7 +4,7 @@
 :Box Type: On-Box
 :Title:Test Script with modification by Gopal
 :Short Description:This script to monitor System resources and modified.
-:Long Description:This Script to monitor System resources(modified) like 
+:Long Description:This Script to monitor System resources(modified now) like 
 CPU, Memory, Buffers.
 :Input:System IDs
 :Output:CPU, Memory, Buffers Status 
