7374e39ade0deadbbbdceeac620ae5a6e65dcdfc
Arun Mittal <mittal.talk@gmail.com>
2015-08-06 15:06:54 +0530

    Update sample_puppet.pp

--- nexusscripts/on-box/config-mgmt/sample_puppet.pp
+++ nexusscripts/on-box/config-mgmt/sample_puppet.pp
@@ -1,10 +1,10 @@
-# Script Cataloging Information
-# Product Info:Nexus::9000::9516::NX-OS Release 6.2
-# Category:Config-Mgmt
-# Box Type:On-Box
-# Title:VLAN Configuration Management puppet
-# Short Description:This script is to perform VLAN operations
-# Long Description:This script is to perform configuration operations
-# of VLAN interfaces
-# Input:N9K Address, username, password, VLAN parameters
-# Output:status/result of the VLAN configuration parameters
+# :Script Cataloging Information
+# :Product Info:Nexus::9000::9516::NX-OS Release 6.2
+# :Category:Config-Mgmt
+# :Box Type:On-Box
+# :Title:VLAN Configuration Management puppet
+# :Short Description:This script is to perform VLAN operations
+# :Long Description:This script is to perform configuration operations
+#  of VLAN interfaces
+# :Input:N9K Address, username, password, VLAN parameters
+# :Output:status/result of the VLAN configuration parameters
