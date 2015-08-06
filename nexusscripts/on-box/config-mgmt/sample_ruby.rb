f36c0c81fef3a42d1c70d67c79e6d7df8229dca2
Arun Mittal <mittal.talk@gmail.com>
2015-08-06 14:28:45 +0530

    Create sample_ruby.rb

---
+++ nexusscripts/on-box/config-mgmt/sample_ruby.rb
@@ -0,0 +1,12 @@
+=begin
+Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:Config-Mgmt
+:Box Type:On-Box
+:Title:VLAN Configuration Management ruby
+:Short Description:This script is to perform VLAN operations
+:Long Description:This script is to perform configuration operations
+of VLAN interfaces
+:Input:N9K Address, username, password, VLAN parameters
+:Output:status/result of the VLAN configuration parameters
+=end
